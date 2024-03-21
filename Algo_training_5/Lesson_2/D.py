5b0c6e9a3f97600f35ac4b1bfb9a038150d1d254
Dmitriy Ushakov Air <udv8@ya.ru>
2024-03-21 18:31:22 +0300

    Шахматная доска

--- Algo_training_5/Lesson_2/D.py
+++ Algo_training_5/Lesson_2/D.py
@@ -3,32 +3,48 @@
  фигуру без дыр. Требуется определить ее периметр."""
 
 
-def calculate_perimeter(cells):
-    perimeter = 0
-    for cell in cells:
-        row, col = cell
-        perimeter += 4
-
-        if (row - 1, col) in cells:
-            perimeter -= 1
-        if (row + 1, col) in cells:
-            perimeter -= 1
-        if (row, col - 1) in cells:
-            perimeter -= 1
-        if (row, col + 1) in cells:
-            perimeter -= 1
-
-    return perimeter
-
+# def calculate_perimeter(cells):
+#     perimeter = 0
+#     for cell in cells:
+#         row, col = cell
+#         perimeter += 4
+#
+#         if (row - 1, col) in cells:
+#             perimeter -= 1
+#         if (row + 1, col) in cells:
+#             perimeter -= 1
+#         if (row, col - 1) in cells:
+#             perimeter -= 1
+#         if (row, col + 1) in cells:
+#             perimeter -= 1
+#
+#     return perimeter
+#
+#
+# n = int(input())
+# cells = []
+# for _ in range(n):
+#     row, col = map(int, input().split())
+#     cells.append((row, col))
+#
+#
+# perimeter = calculate_perimeter(cells)
+# print(perimeter)
 
 n = int(input())
-cells = []
+field = [[0] * 10 for _ in range(10)]
+
 for _ in range(n):
-    row, col = map(int, input().split())
-    cells.append((row, col))
+    i, j = map(int, input().split())
+    field[i][j] = 1
 
+perimeter = 0
 
-perimeter = calculate_perimeter(cells)
+for i in range(1, 10):
+    for j in range(1, 10):
+        perimeter += field[i][j -1 ] != field[i][j]
+        perimeter += field[i - 1][j] != field[i][j]
+
 print(perimeter)
 
 
